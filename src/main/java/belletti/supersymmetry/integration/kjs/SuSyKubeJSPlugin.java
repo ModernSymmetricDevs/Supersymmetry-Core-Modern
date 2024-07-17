ddad71509dfd4ec004128aa94a2cd92f68febffd
Felipe <40951999+fex0r@users.noreply.github.com>
2024-07-17 02:19:13 -0300

    feat: kubejs integration

---
+++ src/main/java/belletti/supersymmetry/integration/kjs/SuSyKubeJSPlugin.java
@@ -0,0 +1,26 @@
+package belletti.supersymmetry.integration.kjs;
+
+import belletti.supersymmetry.Supersymmetry;
+import belletti.supersymmetry.common.data.SuSyBlocks;
+import belletti.supersymmetry.common.data.SuSyItems;
+import belletti.supersymmetry.common.data.SuSyMachines;
+import belletti.supersymmetry.common.data.SuSyRecipeTypes;
+import belletti.supersymmetry.common.data.materials.SuSyElementMaterials;
+import belletti.supersymmetry.common.data.materials.SuSyMaterials;
+import dev.latvian.mods.kubejs.KubeJSPlugin;
+import dev.latvian.mods.kubejs.script.BindingsEvent;
+
+public class SuSyKubeJSPlugin extends KubeJSPlugin {
+    @Override
+    public void registerBindings(BindingsEvent event) {
+        super.registerBindings(event);
+        event.add("SuSyMaterials", SuSyMaterials.class);
+        event.add("SuSyElements", SuSyElementMaterials.class);
+        event.add("SuSyBlocks", SuSyBlocks.class);
+        event.add("SuSyMachines", SuSyMachines.class);
+        event.add("SuSyItems", SuSyItems.class);
+        event.add("SuSyRecipeTypes", SuSyRecipeTypes.class);
+
+        event.add("SuSyCore", Supersymmetry.class);
+    }
+}
